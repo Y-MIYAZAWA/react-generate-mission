4337642e78ffb67d8ca288bb730e952fdeb33c4d
Y-MIYAZAWA <yuki_miyazawa@respawn.jp>
2024-03-18 15:27:02 +0900

    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Input } from './features/batchInput/components/Input'
 
 const App = () => {
     return (
         <div>
             <h1>React実践課題</h1>
-            <Input />
         </div>
     )
-}
+};
 
-export default App
+export default App;
